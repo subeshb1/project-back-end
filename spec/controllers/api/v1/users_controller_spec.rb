a3977785105669baac60705802004ad9256f3903
Subesh Bhandari <subeshb1@gmail.com>
2019-05-15 13:00:11 +0545

    Add spec

---
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+require 'rails_helper'
+
+describe Api::V1::UsersController, type: :api do
+  before do
+    api_key = FactoryBot.create(:api_key)
+    header 'x-api-token', api_key.token
+    header 'Content-type', 'application/json'
+  end
+
+  describe '#create' do
+    subject { post 'api/v1/users', params.to_json }
+    let(:params) do 
+      {
+        email: 'example@gmail.com',
+        password: '12345678',
+        confirm_password: '12345678',
+        type: 'job_seeker'
+      }
+    end
+    context 'when required params are not provided' do
+      let(:params) { base_params.except(:review_status) }
+
+      include_examples 'Validation Failure' do
+        let(:errors) do
+          [
+            { field: 'email', message: 'is missing' },
+            { field: 'password', message: 'is missing' },
+            { field: 'confirm_password', message: 'is missing' },
+            { field: 'job_seeker', message: 'is missing' },
+          ]
+        end
+      end
+    end
+  end
+end
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,7 +1,13 @@
 # frozen_string_literal: true
+
 FactoryBot.define do
   factory :api_key do
-    token {'1ic3Xb1K84uifO82CL5F'}
+    token { '1ic3Xb1K84uifO82CL5F' }
     app_info { { app_name: 'consumer_app' } }
+  end
+
+  factory :user do
+    email { 'example@gmail.com' }
+    role { User::ROLE.key('job_seeker') }
   end
 end
