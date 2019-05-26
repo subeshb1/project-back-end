4b73c3a0e4d22bd5975e01916db00dc6c2c09af8
Subesh Bhandari <subeshb1@gmail.com>
2019-05-26 17:55:07 +0545

    Add heroku updates

--- lib/helper_methods.rb
+++ lib/helper_methods.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
-require 'faker'
-  def create_a_user(params = {})
-    user_params = {
-      email: Faker::Internet.email,
-      password: '123456',
-      password_confirmation: '123456',
-      role: 0
-    }
-    User.create!(user_params.merge!(params))
-  end
+def create_a_user(params = {})
+  require 'faker'
+  user_params = {
+    email: Faker::Internet.email,
+    password: '123456',
+    password_confirmation: '123456',
+    role: 0
+  }
+  User.create!(user_params.merge!(params))
+end
