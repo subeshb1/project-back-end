bc7a6f4de940d55c3596294f3c014403085b10ba
Subesh Bhandari <subeshb1@gmail.com>
2020-09-01 13:14:24 +0545

    Update buildspec

--- cdk/lib/stacks/pipeline-stack.ts
+++ cdk/lib/stacks/pipeline-stack.ts
@@ -2,9 +2,11 @@ import { SecretValue, Stack, Construct, StackProps } from '@aws-cdk/core';
 import * as codepipelineActions from '@aws-cdk/aws-codepipeline-actions';
 import * as codepipeline from '@aws-cdk/aws-codepipeline';
 import * as codebuild from '@aws-cdk/aws-codebuild';
-
+import * as s3 from '@aws-cdk/aws-s3';
+import * as iam from '@aws-cdk/aws-iam';
 export interface PipelineStackProps extends StackProps {
   readonly envType: string;
+  readonly accountId: string;
 }
 export class PipeLineStack extends Stack {
   constructor(scope: Construct, id: string, props?: PipelineStackProps) {
@@ -18,25 +20,53 @@ export class PipeLineStack extends Stack {
         privileged: true,
         environmentVariables: {
           "ENV_TYPE": {
-            value: "test",
+            value: props?.envType,
+          },
+          "ACCOUNT_ID": {
+            value: props?.accountId,
           }
         }
       },
       buildSpec: codebuild.BuildSpec.fromObject({
         version: '0.2',
         phases: {
-          build: {
+          install: {
             commands: [
               'docker volume create --name=postgres-volume',
               'docker-compose up -d postgres',
-              'ENV_TYPE=test docker-compose up create-db',
+              'RAILS_ENV=test docker-compose up create-db',
+              "printenv"
+            ]
+          },
+          build: {
+            commands: [
+              "echo 'RUNNING SPECS'",
               'docker-compose up test',
+              "echo 'Building artifacts'",
+              'export VERSION=$(cat .version)',
+              'docker build . -t ${ENV_TYPE}-back-end:${VERSION}',
+              'cd cdk && npm run build && cdk synth'
             ],
           },
+          post_build: {
+            commands: [
+              "docker tag ${ENV_TYPE}-back-end:${VERSION} ${ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com/${ENV_TYPE}-back-end:${VERSION}",
+              "docker push ${ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com/${ENV_TYPE}-back-end:${VERSION}"
+            ]
+          }
+        },
+        artifacts: {
+          files: [
+            "cdk/cdk.out/*"
+          ]
         }
       })
     })
+
     new codepipeline.Pipeline(this, `${props?.envType}-Pipeline`, {
+      artifactBucket: new s3.Bucket(this, `${props?.envType}-Bucket`, {
+        encryption: s3.BucketEncryption.UNENCRYPTED,
+      }),
       stages: [
         {
           stageName: "GithubSource",
