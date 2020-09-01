535748831cd0170dde0575b489f2a7973ab1efea
Subesh Bhandari <subeshb1@gmail.com>
2020-09-01 13:14:14 +0545

    Pass account ID

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -7,9 +7,13 @@ const app = new cdk.App();
 if (!process.env.ENV_TYPE) {
   throw new Error("ENV_TYPE not specified .")
 }
+if (!process.env.ACCOUNT_TYPE) {
+  throw new Error("ACCOUNT_TYPE not specified .")
+}
 
 new PipeLineStack(app, 'PipeLineStack', {
-  envType: process.env.ENV_TYPE
+  envType: process.env.ENV_TYPE,
+  accountId: process.env.ACCOUNT_TYPE,
 });
 new InfrastructureStack(app, 'InfrastructureStack', {
   // envType: process.env.ENV_TYPE
