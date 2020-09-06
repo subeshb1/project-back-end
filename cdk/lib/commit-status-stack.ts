a9f463b4ef1edb3d4ca700c7afe3bdaeed30226a
Subesh Bhandari <subeshb1@gmail.com>
2020-09-06 15:12:46 +0545

    Add commit status lambda

--- cdk/lib/commit-status-stack.ts
+++ cdk/lib/commit-status-stack.ts
@@ -1,25 +1,33 @@
-import * as cdk from '@aws-cdk/core';
-import * as lambda from '@aws-cdk/aws-lambda';
-import { Rule, Schedule } from '@aws-cdk/aws-events';
-import * as targets from '@aws-cdk/aws-events-targets';
-import * as path from 'path';
+import * as cdk from "@aws-cdk/core";
+import * as lambda from "@aws-cdk/aws-lambda";
+import { Rule, Schedule } from "@aws-cdk/aws-events";
+import * as sns from "@aws-cdk/aws-sns";
+import * as path from "path";
+import { SnsEventSource } from "@aws-cdk/aws-lambda-event-sources";
 export class CommitStatusStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props: cdk.StackProps) {
     super(scope, id, props);
 
-    const githubNotifyLambda = new lambda.Function(this, 'GithubNotifier', {
+    const githubNotifyLambda = new lambda.Function(this, "GithubNotifier", {
       runtime: lambda.Runtime.NODEJS_10_X,
-      handler: 'index.handler',
+      handler: "index.handler",
       functionName: "GithbCommitStatus",
-      code: lambda.Code.fromAsset(path.join(__dirname, 'resources/commit-status-lambda')),
-    })
-    // new Rule(this, 'CodePipelineAlert', {
-    //   eventPattern: {
-    //     source: [
-    //       "aws.codepipeline"
-    //     ]
-    //   },
-    //   targets: [new targets.LambdaFunction(githubNotifyLambda)]
-    // })
+      code: lambda.Code.fromAsset(
+        path.join(__dirname, "resources/commit-status-lambda")
+      ),
+    });
+
+    const pipelineStatusSNS = new sns.Topic(this, "PipelineStatusTopic", {
+      displayName: "PipeLineStatusTopic",
+      topicName: "pipeline-status-topic",
+    });
+
+
+    new cdk.CfnOutput(this, "PipelineStatusTopicOutput", {
+      exportName: "pipeLineStatusTopicArnOutput",
+      value: pipelineStatusSNS.topicArn,
+    });
+
+    githubNotifyLambda.addEventSource(new SnsEventSource(pipelineStatusSNS));
   }
 }
