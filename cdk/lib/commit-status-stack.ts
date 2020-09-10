af0aa0cc7721788c7f13917d0d55784c4d8f40f7
Subesh Bhandari <subeshb1@gmail.com>
2020-09-10 22:07:38 +0545

    Integrate sqs with lambda and sns

--- cdk/lib/commit-status-stack.ts
+++ cdk/lib/commit-status-stack.ts
@@ -2,9 +2,11 @@ import * as cdk from "@aws-cdk/core";
 import * as lambda from "@aws-cdk/aws-lambda";
 import { Rule, Schedule } from "@aws-cdk/aws-events";
 import * as sns from "@aws-cdk/aws-sns";
+import * as sqs from "@aws-cdk/aws-sqs";
 import * as iam from "@aws-cdk/aws-iam";
 import * as path from "path";
-import { SnsEventSource } from "@aws-cdk/aws-lambda-event-sources";
+import { SqsSubscription } from "@aws-cdk/aws-sns-subscriptions";
+import { SqsEventSource } from "@aws-cdk/aws-lambda-event-sources";
 export class CommitStatusStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props: cdk.StackProps) {
     super(scope, id, props);
@@ -23,15 +25,27 @@ export class CommitStatusStack extends cdk.Stack {
       topicName: "pipeline-status-topic",
     });
 
+    const notificationQueue = new sqs.Queue(this, "PipelineNotificationQueue", {
+      fifo: false,
+    });
+
+    // notificationQueue.grantSendMessages({
+    //   grantPrincipal: new iam.ArnPrincipal(pipelineStatusSNS.topicArn),
+    // });
+
+    pipelineStatusSNS.addSubscription(new SqsSubscription(notificationQueue));
+
     pipelineStatusSNS.grantPublish({
-      grantPrincipal: new iam.ServicePrincipal('codestar-notifications.amazonaws.com')
-    })
+      grantPrincipal: new iam.ServicePrincipal(
+        "codestar-notifications.amazonaws.com"
+      ),
+    });
 
     new cdk.CfnOutput(this, "PipelineStatusTopicOutput", {
       exportName: "pipeLineStatusTopicArnOutput",
       value: pipelineStatusSNS.topicArn,
     });
 
-    githubNotifyLambda.addEventSource(new SnsEventSource(pipelineStatusSNS));
+    githubNotifyLambda.addEventSource(new SqsEventSource(notificationQueue));
   }
 }
