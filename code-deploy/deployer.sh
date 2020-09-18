fa63dd3bf249d4423c47e868e3bbd1a5da553e3a
Subesh Bhandari <subeshb1@gmail.com>
2020-09-18 19:05:26 +0545

    Add task def generation script

--- code-deploy/deployer.sh
+++ code-deploy/deployer.sh
@@ -2,7 +2,6 @@
 
 set -e
 
-EXPORT_NAME=$(jq -r ".Exports | map(select(.Name == \"$ENV_TYPE-TaskDefinitionArn\")) | .[0].Value" <<< $(aws --profile=college cloudformation list-exports))
+TASK_DEF_ARN=$(jq -r ".Exports | map(select(.Name == \"$ENV_TYPE-TaskDefinitionArn\")) | .[0].Value" <<< $(aws cloudformation list-exports))
 
-sed -i '' "s/TASK_DEFINITION_ARN_PLACEHOLDER/$EXPORT_NAME/" appspec.yaml
-sed -i '' "s/TASK_DEFINITION_ARN_PLACEHOLDER/$EXPORT_NAME/" taskdef.json
+jq '.taskDefinition | del(.taskDefinitionArn) | .image = "<IMAGE_PLACEHOLDER>" ' <<<$(aws ecs describe-task-definition --task-definition $TASK_DEF_ARN) > taskdef.json
