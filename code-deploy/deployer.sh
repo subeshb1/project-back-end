0785c226baedd3e3799b2d1cc4648a7cf58e5d05
Subesh Bhandari <subeshb1@gmail.com>
2020-09-18 19:53:18 +0545

    Trigger build

--- code-deploy/deployer.sh
+++ code-deploy/deployer.sh
@@ -1,9 +1,14 @@
 #!/bin/bash  
 
-TASK_DEF_ARN=$(jq -r ".Exports | map(select(.Name == \"${ENV_TYPE}TaskDefinitionArn\")) | .[0].Value" <<< $(aws cloudformation list-exports))
+echo "ENV_TYPE: $ENV_TYPE"
+echo "ENV_NAME: ${ENV_TYPE}TaskDefinitionArn"
+
+aws --region $AWS_DEFAULT_REGION cloudformation list-exports
+
+TASK_DEF_ARN=$(jq -r ".Exports | map(select(.Name == \"${ENV_TYPE}TaskDefinitionArn\")) | .[0].Value" <<< $(aws --region $AWS_DEFAULT_REGION cloudformation list-exports))
 
 echo $TASK_DEF_ARN
 
-aws ecs describe-task-definition --task-definition $TASK_DEF_ARN
+aws--region $AWS_DEFAULT_REGION   ecs describe-task-definition --task-definition $TASK_DEF_ARN
 
-jq '.taskDefinition | del(.taskDefinitionArn) | .image = "<IMAGE_PLACEHOLDER>" ' <<<$(aws ecs describe-task-definition --task-definition $TASK_DEF_ARN) > taskdef.json
+jq '.taskDefinition | del(.taskDefinitionArn) | .image = "<IMAGE_PLACEHOLDER>" ' <<<$(aws --region $AWS_DEFAULT_REGION  ecs describe-task-definition --task-definition $TASK_DEF_ARN) > taskdef.json
