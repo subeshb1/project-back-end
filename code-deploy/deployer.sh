ede0c46f59746c71bb78ec8a71f238ee2dbcd538
Subesh Bhandari <subeshb1@gmail.com>
2020-09-18 19:33:37 +0545

    Update deployer code

--- code-deploy/deployer.sh
+++ code-deploy/deployer.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-TASK_DEF_ARN=$(jq -r ".Exports | map(select(.Name == \"$ENV_TYPE-TaskDefinitionArn\")) | .[0].Value" <<< $(aws cloudformation list-exports))
+TASK_DEF_ARN=$(jq -r ".Exports | map(select(.Name == \"${ENV_TYPE}TaskDefinitionArn\")) | .[0].Value" <<< $(aws cloudformation list-exports))
 
 jq '.taskDefinition | del(.taskDefinitionArn) | .image = "<IMAGE_PLACEHOLDER>" ' <<<$(aws ecs describe-task-definition --task-definition $TASK_DEF_ARN) > taskdef.json
