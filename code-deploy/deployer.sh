fb9d813901e1983fd499d20991190814718655b3
Subesh Bhandari <subeshb1@gmail.com>
2020-09-18 18:43:33 +0545

    Update deployer

--- code-deploy/deployer.sh
+++ code-deploy/deployer.sh
@@ -4,4 +4,5 @@ set -e
 
 EXPORT_NAME=$(jq -r ".Exports | map(select(.Name == \"$ENV_TYPE-TaskDefinitionArn\")) | .[0].Value" <<< $(aws --profile=college cloudformation list-exports))
 
-sed "s/TASK_DEFINITION_ARN_PLACEHOLDER/$EXPORT_NAME/" appspec.template.yaml > appspec.yaml
+sed -i '' "s/TASK_DEFINITION_ARN_PLACEHOLDER/$EXPORT_NAME/" appspec.yaml
+sed -i '' "s/TASK_DEFINITION_ARN_PLACEHOLDER/$EXPORT_NAME/" taskdef.json
