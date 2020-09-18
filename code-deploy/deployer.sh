642d230880736025ae7d9b1c61d5749dd748c6a7
Subesh Bhandari <subeshb1@gmail.com>
2020-09-18 13:23:53 +0545

    Update deployer script

--- code-deploy/deployer.sh
+++ code-deploy/deployer.sh
@@ -1,1 +1,7 @@
-jq ".Exports | map(select(.Name == \"$ENV_TYPE-TaskDefinitionArn\")) | .[0].Value' <<< $(aws cloudformation list-exports)
+#!/bin/bash  
+
+set -e
+
+EXPORT_NAME=$(jq -r ".Exports | map(select(.Name == \"$ENV_TYPE-TaskDefinitionArn\")) | .[0].Value" <<< $(aws --profile=college cloudformation list-exports))
+
+sed "s/TASK_DEFINITION_ARN_PLACEHOLDER/$EXPORT_NAME/" appspec.template.yaml > appspec.yaml
