796c699193a96835bc5eb35fb01a89b44a8941f0
Subesh Bhandari <subeshb1@gmail.com>
2020-09-18 20:16:12 +0545

    Update deployer

--- code-deploy/deployer.sh
+++ code-deploy/deployer.sh
@@ -1,14 +1,7 @@
 #!/bin/bash  
 
-echo "ENV_TYPE: $ENV_TYPE"
-echo "ENV_NAME: ${ENV_TYPE}-TaskDefinitionArn"
-
 aws --region $AWS_DEFAULT_REGION cloudformation list-exports
 
 TASK_DEF_ARN=$(jq -r ".Exports | map(select(.Name == \"${ENV_TYPE}-TaskDefinitionArn\")) | .[0].Value" <<< $(aws --region $AWS_DEFAULT_REGION cloudformation list-exports))
-
-echo $TASK_DEF_ARN
-
-aws --region $AWS_DEFAULT_REGION ecs describe-task-definition --task-definition $TASK_DEF_ARN
 
 jq '.taskDefinition | del(.taskDefinitionArn) | .image = "<IMAGE_PLACEHOLDER>" ' <<<$(aws --region $AWS_DEFAULT_REGION  ecs describe-task-definition --task-definition $TASK_DEF_ARN) > taskdef.json
